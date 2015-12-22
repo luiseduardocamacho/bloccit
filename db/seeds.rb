39d8c081e05b8f106ae3e2aea5021bf10a8ffd46
Luis Camacho <luiscamacho@gmail.com>
2015-12-21 20:30:53 -0500

    Assignment:30 - Added idempotent code to the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,19 @@ include RandomData
    )
  end
 
+ Post.create!(title:'Testing Creating without find_or_create_by',
+              body: 'This is a test body')
+
+ Post.find_or_create_by(title:'SEC to Pursue Amended Civil Case Against Hedge-Fund Billionaire Steven Cohen') do |post|
+  post.body = 'The Securities and Exchange Commission said it would pursue an amended civil
+  case against hedge-fund billionaire Steven Cohen, after criminal prosecutors were forced
+     to drop a related insider-trading case against one of his employees.'
+     end
+
+     Comment.find_or_create_by(
+     post: posts.find_by(title:'SEC to Pursue Amended Civil Case Against Hedge-Fund Billionaire Steven Cohen'),
+     body: 'testing')
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
