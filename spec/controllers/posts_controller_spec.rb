cea3c4a223e39aeb715ef8c2c311c6fa72834932
Luis Camacho <luiscamacho@gmail.com>
2016-01-04 21:08:08 -0500

    Added expectations to Post show view

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,14 +12,17 @@ RSpec.describe PostsController, type: :controller do
 
        it "returns http success" do
          get :show, topic_id: my_topic.id, id: my_post.id
+         expect(response).to have_http_status(:success)
        end
 
        it "renders the #show view" do
          get :show, topic_id: my_topic.id, id: my_post.id
+         expect(response).to render_template :show
        end
 
        it "assigns my_post to @post" do
           get :show, topic_id: my_topic.id, id: my_post.id
+          expect(assigns(:post)).to eq(my_post)
         end
       end
 
