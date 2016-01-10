1d45aa997481ada472dc0c03a3c264f218046cd5
Luis Camacho <luiscamacho@gmail.com>
2016-01-10 13:34:02 -0500

    Checkpoint-38: Added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Bloccit::Application.routes.draw do
      resources :posts, except: [:index]
    end
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
   root 'welcome#index'
 
