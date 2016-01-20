e324d6bf5969873ea4c21e5ae3cccef36a001824
Luis Camacho <luiscamacho@gmail.com>
2016-01-19 21:01:50 -0500

    Checkpoint-41: Added comments controller as well as two comment views for functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Bloccit::Application.routes.draw do
   resources :topics do
      resources :posts, except: [:index]
    end
+
+   resources :posts, only: [] do
+        resources :comments, only: [:create, :destroy]
+      end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
