f6fda64445722d63d60d7b92f16c5a913aa27c3a
Luis Camacho <luiscamacho@gmail.com>
2016-01-23 15:47:55 -0500

    Checkpoint-43: Added Voting functionality to Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,16 @@ users = User.all
 
   # Create Posts
  50.times do
- # #1
-   Post.create!(
- # #2
+ post = Post.create!(
      user:  users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+
+post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+rand(1..5).times {post.votes.create!(value: [-1,1].sample, user: users.sample)}
  end
  posts = Post.all
 
@@ -67,3 +69,4 @@ admin = User.create!(
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
