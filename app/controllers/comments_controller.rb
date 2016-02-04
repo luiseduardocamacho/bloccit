7e834c8168a34557836b15050cb29102a2a38351
Luis Camacho <luiscamacho@gmail.com>
2016-02-03 19:21:44 -0500

    Checkpoint-47: WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,34 +2,37 @@ class CommentsController < ApplicationController
   before_action :require_sign_in
   before_action :authorize_user, only: [:destroy]
    def create
- # #11
+
      @post = Post.find(params[:post_id])
      comment = @post.comments.new(comment_params)
      comment.user = current_user
 
      if comment.save
        flash[:notice] = "Comment saved successfully."
- # #12
+
        redirect_to [@post.topic, @post]
      else
        flash[:alert] = "Comment failed to save."
- # #13
+
        redirect_to [@post.topic, @post]
      end
    end
 
    def destroy
        @post = Post.find(params[:post_id])
-       comment = @post.comments.find(params[:id])
+       @comment = @post.comments.find(params[:id])
 
-       if comment.destroy
+       if @comment.destroy
          flash[:notice] = "Comment was deleted."
-         redirect_to [@post.topic, @post]
        else
          flash[:alert] = "Comment couldn't be deleted. Try again."
-         redirect_to [@post.topic, @post]
        end
-     end
+
+       respond_to do |format|
+         format.html
+         format.js
+       end
+   end
 
 
    private
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -20,7 +20,7 @@ include RandomData
 
      describe "DELETE destroy" do
        it "redirects the user to the sign in view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          expect(response).to redirect_to(new_session_path)
        end
      end
@@ -45,7 +45,7 @@ include RandomData
 
      describe "DELETE destroy" do
        it "redirects the user to the posts show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          expect(response).to redirect_to([my_topic, my_post])
        end
      end
@@ -71,16 +71,16 @@ include RandomData
 
      describe "DELETE destroy" do
        it "deletes the comment" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          count = Comment.where({id: my_comment.id}).count
          expect(count).to eq 0
        end
 
-       it "redirects to the post show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to [my_topic, my_post]
+       it "returns http success" do
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+         expect(response).to have_http_status(:success)
        end
-     end
+      end
    end
 
  # #9
@@ -103,14 +103,14 @@ include RandomData
 
      describe "DELETE destroy" do
        it "deletes the comment" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          count = Comment.where({id: my_comment.id}).count
          expect(count).to eq 0
        end
 
        it "redirects to the post show view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
-         expect(response).to redirect_to [my_topic, my_post]
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+         expect(response).to have_http_status(:success)
        end
      end
    end
