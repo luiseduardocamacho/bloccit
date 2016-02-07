5a83e3bb7f856e0f43775f68de59143bda5c8ffb
Luis Camacho <luiscamacho@gmail.com>
2016-02-07 11:12:07 -0500

    Checkpoint-49: Added update and create functionality to the User API and create update and destroy to the Topic API

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Bloccit::Application.routes.draw do
 
   namespace :api do
      namespace :v1 do
-       resources :users, only: [:index, :show]
-       resources :topics, only: [:index, :show]
+       resources :users, only: [:index, :show, :create, :update]
+       resources :topics, only: [:index, :show, :update, :create, :destroy]
      end
    end
 end
